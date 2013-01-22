c941fc5dab71f0bc24e8102765ae59de171de95d
Clive Walkden <clivewalkden@gmail.com>
2013-01-22 18:58:34 +0000

    Initial Commit

---
+++ js/jquery.cw_hidereveal-0.1.js
@@ -0,0 +1,47 @@
+/*!
+ * Hide Reveal - for jQuery 1.7+
+ * http://www.cwenterprises.co.uk
+ *
+ * Copyright 2013, Clive Walkden (http://www.cwenterprises.co.uk)
+ *
+ * @package Hide Reveal
+ * @author Clive Walkden (http://www.cwenterprises.co.uk)
+ * @version 1.0.0
+ * @copyright Copyright (c) 2012 SOZO Design Ltd (http://www.cwenterprises.co.uk)
+ * @date: 22-01-2013
+ */
+
+(function ($) {
+    $.fn.CWHideReveal = function (settings) {
+
+        // Default plugin settings
+        var defaults = {
+            speed: 300,
+            easing: '',
+            changeText: false,
+            showText: 'Show',
+            hideText: 'Hide'
+        };
+
+        // Merge default and user settings
+        var settings = $.extend({}, defaults, settings);
+
+        $(this).click(function () {
+            // Add class cw_close to any div you want to automatically close
+            $('.cw_close').slideUp(settings.speed, settings.easing);
+
+            var object = $(this);
+
+            var object_id = object.data('id');
+
+            $(object_id).slideToggle(settings.speed, settings.easing, function() {
+
+                if(settings.changeText==1){
+                    $(object_id).is(":visible") ? object.text(settings.hideText) : object.text(settings.showText);
+                }
+            });
+
+            return false;
+        });
+    };
+})(jQuery);
