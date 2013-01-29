71bb4b758d554d5472fd86f18086449983173665
Clive Walkden <clivewalkden@gmail.com>
2013-01-29 18:12:34 +0000

    Finalised the accordian Updated the examples

--- js/jquery.cw_hidereveal-0.1.js
+++ js/jquery.cw_hidereveal-0.1.js
@@ -9,6 +9,7 @@
  * @version 1.0.0
  * @copyright Copyright (c) 2012 SOZO Design Ltd (http://www.cwenterprises.co.uk)
  * @date: 22-01-2013
+ * @todo Tidy up the script now that it works
  */
 
 (function ($) {
@@ -47,20 +48,32 @@
                 e.preventDefault();
 
                 // If accordian is set automatically close all open divs
-                // NOT SURE THIS WORKS HERE
-                // Exclude the
                 if(settings.accordian==true){
-                    if(settings.changeText==true){
-                        $('.'+settings.openClass).is(":visible") ? link.text(settings.hideText) : link.text(settings.showText);
-                    }
+
                     $('.'+settings.openClass).slideUp(settings.speed, settings.easing,function(){
                         $('.'+settings.openClass).removeClass(settings.openClass);
                     });
+
+                    if ($(container).is(':hidden')) {
+
+                        $(container).slideDown(settings.speed, settings.easing, function(){
+
+                            if(settings.changeText==true){
+                                $('.'+settings.openClass).is(":visible") ? $(this).text(settings.hideText) : $(this).text(settings.showText);
+                            }
+
+                            $(this).addClass(settings.openClass);
+                        });
+                    }
+
                 }else{
+
                     $(container).slideToggle(settings.speed, settings.easing, function() {
+
                         if(settings.changeText==true){
                             $(container).is(":visible") ? link.text(settings.hideText) : link.text(settings.showText);
                         }
+
                         if ($(this).hasClass(settings.openClass)) {
                             $(this).removeClass(settings.openClass);
                         }else{
@@ -69,7 +82,7 @@
                     });
                 }
 
-                return false;
+                return this;
             });
         });
     };
